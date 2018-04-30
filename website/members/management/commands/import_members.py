e75ddc67cc07f13d9492288c889dcd8669cd05a2
Carlos Matías de la Torre <cdelatorre@machinalis.com>
2018-04-30 18:30:25 -0300

    update import script

--- website/members/management/commands/import_members.py
+++ website/members/management/commands/import_members.py
@@ -4,7 +4,10 @@ from datetime import datetime
 
 from django.core.management.base import BaseCommand, CommandError
 
-from members.models import Person, Member, Category
+from members.models import Person, Member, Category, Patron
+
+
+CAT_ESTUDIANTE = 'Estudiante'
 
 
 class Command(BaseCommand):
@@ -14,27 +17,28 @@ class Command(BaseCommand):
         parser.add_argument('filename', type=str)
 
     def handle(self, *args, **options):
-        if options['filename'] == None:
+        if options['filename'] is None:
             raise CommandError("You must specify the path of file.")
 
         # make sure file path resolves
         if not os.path.isfile(options['filename']):
-            raise CommandError("File has no exists.")
+            raise CommandError("File does not exists.")
 
         dataReader = csv.reader(open(options["filename"]), delimiter=',', quotechar='"')
         for member_value in dataReader:
-            person = self.get_or_create_person(member_value)
+            person, patron = self.get_or_create_person_and_patron(member_value)
             category = self.get_or_create_category(member_value)
-            member, created = Member.objects.update_or_create(
+            person.membership, created = Member.objects.update_or_create(
                 legal_id=member_value[0].strip(),
                 defaults={
                     'registration_date': datetime.strptime(member_value[1].strip(), "%d/%M/%Y"),
                     'category': category,
-                    'person': person,
-                    'has_student_certificate': True if category.name == 'Estudiante' else False,
+                    'patron': patron,
+                    'has_student_certificate': True if category.name == CAT_ESTUDIANTE else False,
                     'has_subscription_letter': True
                 })
-            self.stdout.write("Member imported: {})".format(member))
+            person.save()
+            self.stdout.write("Member imported: {}".format(person.membership))
 
     def get_or_create_category(self, values):
         category, created = Category.objects.get_or_create(
@@ -45,7 +49,7 @@ class Command(BaseCommand):
             })
         return category
 
-    def get_or_create_person(self, values):
+    def get_or_create_person_and_patron(self, values):
         person, created = Person.objects.update_or_create(
             first_name=values[3].strip(),
             last_name=values[4].strip(),
@@ -59,4 +63,12 @@ class Command(BaseCommand):
                 'birth_date': datetime.strptime(values[13].strip(), "%d/%M/%Y"),
                 'street_address': values[14].strip()
             })
-        return person
+
+        patron, created = Patron.objects.update_or_create(
+            name=person.full_name,
+            email=person.email,
+            defaults={
+                'comments': 'Automatically loaded with PyCAmp 2018 script',
+            })
+
+        return person, patron
