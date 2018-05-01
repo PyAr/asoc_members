2c5b45698cb50dc4f7e1088d6702ef1d0b5931d6
Matias Varela <matu.varela@gmail.com>
2018-05-01 02:08:02 -0300

    add script to upload people

--- website/members/admin.py
+++ website/members/admin.py
@@ -1,10 +1,30 @@
 from django.contrib import admin
+from django.contrib.admin import SimpleListFilter
 
 from .models import Member, Person, Organization, Patron, Payment, PaymentStrategy, Quota
 
 
+class PersonNoMembers(SimpleListFilter):
+    title = 'person no member' # or use _('country') for translated title
+    parameter_name = 'membership'
+
+    def lookups(self, request, model_admin):
+       return (
+           ('filtered', 'Member in progress'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == 'filtered':
+            return queryset.filter(membership__isnull=True)
+        return queryset
+
+
+class PersonAdmin(admin.ModelAdmin):
+    list_filter = (PersonNoMembers, )
+
+
 admin.site.register(Member)
-admin.site.register(Person)
+admin.site.register(Person, PersonAdmin)
 admin.site.register(Organization)
 admin.site.register(Patron)
 admin.site.register(Payment)
---
+++ website/members/management/commands/import_people.py
@@ -0,0 +1,53 @@
+import csv
+import os
+from datetime import datetime
+
+from django.core.management.base import BaseCommand, CommandError
+
+from members.models import Person, Patron
+
+
+class Command(BaseCommand):
+    help = "Import people from csv generated with data from Google Spreadsheet"
+
+    def add_arguments(self, parser):
+        parser.add_argument('filename', type=str)
+
+    def handle(self, *args, **options):
+        if options['filename'] is None:
+            raise CommandError("You must specify the path of file.")
+
+        # make sure file path resolves
+        if not os.path.isfile(options['filename']):
+            raise CommandError("File does not exists.")
+
+        with open(options["filename"]) as csvfile:
+            reader = csv.DictReader(csvfile)
+            for row in reader:
+                person = self.get_or_create_person_and_patron(row)
+                self.stdout.write("Person imported: {}".format(person))
+
+    def get_or_create_person_and_patron(self, values):
+        person, created = Person.objects.update_or_create(
+            first_name=values['Nombre'].strip(),
+            last_name=values['Apellido'].strip(),
+            email=values['EMail'].strip(),
+            defaults={
+                'document_number': values['DNI'].strip(),
+                'nickname': values['Nick'].strip(),
+                'nationality': values['Nacionalidad'].strip(),
+                'marital_status': values['Estado Civil'].strip(),
+                'occupation': values['Profesión'].strip(),
+                'birth_date': datetime.strptime(values['Fecha Nacimiento'].strip(), "%d/%M/%Y"),
+                'street_address': values['Domicilio'].strip(),
+                'comments': "Tipo de socio elegido: %s" % values["Tipo socio"].strip()
+            })
+
+        Patron.objects.update_or_create(
+            name=person.full_name,
+            email=person.email,
+            defaults={
+                'comments': 'Automatically loaded with PyCAmp 2018 script',
+            })
+
+        return person
