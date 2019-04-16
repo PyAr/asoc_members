711bcaaf8fd5cc0af23920cef3ff47dc13fec19c
Andres Ramirez Miori <andres.ramirez.miori@gmail.com>
2019-04-16 17:34:07 -0300

    signup organizer test and sendmail test

--- website/events/tests.py
+++ website/events/tests.py
@@ -13,13 +13,12 @@ class EmailTest(TestCase):
             email="admin@test.com", 
             password="administrator"
             )
-            
+
     def tearDown(self):
         self.organizer_user.delete()
         self.super_user.delete()
 
     def test_send_email_after_register_organizer(self):
-        
         # Login client with super user
         self.client.login(username='administrator', password='administrator')
 
@@ -34,13 +33,34 @@ class EmailTest(TestCase):
         #TODO: use template here
         self.assertEqual(mail.outbox[0].subject, 'PyAr eventos alta organizador')
 
-    #TODO: move to another test suite
+
+
+class SingnupOrginizerTest(TestCase):
+    def setUp(self):
+        self.organizer_user = User.objects.create_user(username="organizer", email="test@test.com", password="organizer")
+        self.super_user = User.objects.create_superuser(
+            username="administrator", 
+            email="admin@test.com", 
+            password="administrator"
+            )
+    
     def test_organizer_signup_redirects_without_perms(self):
         response = self.client.get(reverse('organizer_signup'))
+        # Login client with not superuser
+        self.client.login(username='organizer', password='organizer')
+
         # View redirect.
         self.assertEqual(response.status_code, 302)
         
         # And redirect to login.
         redirect_to_login_url = reverse('login') + '?next=' + reverse('organizer_signup')
         self.assertEqual(response.url, redirect_to_login_url)
-        
+
+    
+    def test_user_with_add_organizer_perm_no_redirects(self):
+        # Login client with super user
+        self.client.login(username='administrator', password='administrator')
+
+        response = self.client.get(reverse('organizer_signup'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'organizer_signup.html')
