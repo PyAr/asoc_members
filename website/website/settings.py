0116ee02f05e4a2fc04cd38c8eca1926e69d5c13
lecovi <leandro.colombo@mercadolibre.com>
2021-05-22 13:16:39 -0300

    Adds django.contrib.humanize to installed apps

--- website/website/settings.py
+++ website/website/settings.py
@@ -48,6 +48,7 @@ class Base(Configuration):
     INSTALLED_APPS = [
         'django.contrib.admin',
         'django.contrib.auth',
+        'django.contrib.humanize',
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
