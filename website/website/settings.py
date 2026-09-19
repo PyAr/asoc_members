176be0c26fdf323e47d783cdf2152a3d757ac260
lecovi <lecovi@ac.python.org.ar>
2026-09-19 10:21:15 -0300

    fix: set DEFAULT_AUTO_FIELD to resolve W042 system check warnings on Django 4.2

--- website/website/settings.py
+++ website/website/settings.py
@@ -138,6 +138,8 @@ class Base(Configuration):
     # Static files (CSS, JavaScript, Images)
     # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
+    DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
     STATIC_URL = '/static/'
     STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
 
